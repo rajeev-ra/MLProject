b5aabfda1d14ad6c01929cb8300a004d50b86c24
dixitsandeep <dixitsandeep@gmail.com>
2018-04-18 15:17:03 +0530

    After meeting

--- src/main/tests/Election01.py
+++ src/main/tests/Election01.py
@@ -5,7 +5,7 @@ from template.correlation import Correlation
 a = Correlation("./../../../data/Elections/LS-2014_ElectionResult-CandiateWise.xls")
 #a.plot_corr(True)
 
-a.plot_distribution("Candidate Age", [("Position", 1)]);
+a.plot_distribution("Candidate Age", [("Position", 1)],bin=10);
 
 a.plot_distribution_discrete("Candidate Category", title = "Candidates filed nomination")
 
