53255138d48a10023961db2bc33d9cc50454287e
dixitsandeep <dixitsandeep@gmail.com>
2018-04-25 15:48:07 +0530

    Data files

--- src/main/tests/Election01.py
+++ src/main/tests/Election01.py
@@ -3,7 +3,7 @@ sys.path.insert(0,'..')
 from template.correlation import Correlation
 
 a = Correlation("./../../../data/Elections/LS-2014_ElectionResult-CandiateWise.xls")
-#a.plot_corr(True)
+a.plot_corr(True)
 
 a.plot_distribution("Candidate Age", [("Position", 1)],bin=10);
 
